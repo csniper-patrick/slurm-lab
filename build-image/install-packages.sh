5eec67be476addab62bb43bee98fec61a8e3eef8
Patrick Pun <patrickwypun04@gmail.com>
2023-11-20 23:35:22 +0000

    Add jupyter lab to master container

--- build-image/install-packages.sh
+++ build-image/install-packages.sh
@@ -3,11 +3,15 @@
 # Install 
 yum install -y yum-utils epel-release
 yum-config-manager --enable powertools
-yum install -y slurm-{slurmctld,slurmd,slurmdbd,slurmrestd,example-configs} sudo vim man mpich mpich-devel mpich-doc @development gcc-gfortran hwloc pmix pam_script
-yum module install python39
-yum module enable python39-devel
+yum install -y slurm-{slurmctld,slurmd,slurmdbd,slurmrestd,example-configs} sudo vim man mpich mpich-devel mpich-doc @development gcc-gfortran hwloc pmix pam_script tmux
+
+# install jupyterlab
+yum -y module install python39
+yum -y module enable python39-devel
+yum -y install python39-devel
 pip3 install packaging
 pip3 install jupyterlab
+
 yum clean all
 
 # Create users
