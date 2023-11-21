a83a9f4da13e985e74a9361f108f432d33872322
Patrick Pun <patrickwypun04@gmail.com>
2023-11-21 21:14:57 +0000

    run jupyterhub instead of jupyterlab for multi user access

--- build-image/install-packages.sh
+++ build-image/install-packages.sh
@@ -5,13 +5,22 @@ yum install -y yum-utils epel-release
 yum-config-manager --enable powertools
 yum install -y slurm-{slurmctld,slurmd,slurmdbd,slurmrestd,example-configs} sudo vim man mpich mpich-devel mpich-doc @development gcc-gfortran hwloc pmix pam_script tmux
 
-# install jupyterlab
+# install jupyterhub
 yum -y module install python39
 yum -y module enable python39-devel
 yum -y install python39-devel
-pip3 install packaging
-pip3 install jupyterlab bash_kernel
-python3 -m bash_kernel.install
+# pip3 install packaging
+# pip3 install jupyterlab bash_kernel
+# python3 -m bash_kernel.install
+python3 -m venv /opt/jupyterhub/
+/opt/jupyterhub/bin/python3 -m pip install wheel
+/opt/jupyterhub/bin/python3 -m pip install jupyterhub jupyterlab bash_kernel
+/opt/jupyterhub/bin/python3 -m pip install ipywidgets
+/opt/jupyterhub/bin/python3 -m bash_kernel.install
+yum -y module install nodejs:18
+npm install -g configurable-http-proxy
+mkdir -p /opt/jupyterhub/etc/jupyterhub/
+
 yum clean all
 
 # Create users
