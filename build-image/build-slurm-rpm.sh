bb10e1b2effc106c48c83a4efb88f962846f9136
Patrick Pun <patrickwypun04@gmail.com>
2023-11-21 20:11:15 +0000

    build slurm using submodule source

--- build-image/build-slurm-rpm.sh
+++ build-image/build-slurm-rpm.sh
@@ -1,15 +1,11 @@
 #!/bin/bash -x
-version=23.02.5
-
 yum -y install @development yum-utils rpm-build epel-release
-# yum -y install http://repo.openfusion.net/centos8-x86_64/openfusion-release-0.8-1.of.el8.noarch.rpm
 yum-config-manager --enable powertools
 yum -y install munge munge-devel mariadb mariadb-devel gtk2 gtk2-devel gtk3 gtk3-devel http-parser http-parser-devel json-c json-c-devel libyaml libyaml-devel libjwt libjwt-devel wget python3 readline-devel pam-devel perl-ExtUtils-MakeMaker perl-devel perl-JSON-PP createrepo_c hdf5 hdf5-devel man2html man2html-core pam pam-devel freeipmi freeipmi-devel numactl numactl-devel pmix pmix-devel hwloc hwloc-devel
-cd ~
-mkdir ~/source
-cd ~/source
-wget https://download.schedmd.com/slurm/slurm-${version}.tar.bz2
-rpmbuild -ta --with slurmrestd --with hdf5 --with hwloc --with numa --with pmix slurm-${version}.tar.bz2 |& tee build.log
+ver=$(grep "Version:" slurm-src/slurm.spec | head -n 1 | awk '{print $2}' )
+mv slurm-src slurm-${ver}
+tar azcvf slurm-${ver}.tar.bz2 slurm-${ver}
+rpmbuild -ta --with slurmrestd --with hdf5 --with hwloc --with numa --with pmix slurm-${ver}.tar.bz2 |& tee build.log
 cd ~
 mkdir -pv /opt/slurm-repo/Packages
 find /root/rpmbuild/RPMS/ -iname "*.rpm" -exec mv {} /opt/slurm-repo/Packages \;
