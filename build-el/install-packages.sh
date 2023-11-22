187f7c617ff5147a8d66d8ab9be403e89f322887
Patrick Pun <patrickwypun04@gmail.com>
2023-11-22 22:10:17 +0000

    tutorial workspace organize

--- build-el/install-packages.sh
+++ build-el/install-packages.sh
@@ -6,9 +6,7 @@ yum-config-manager --enable powertools
 yum install -y slurm-{slurmctld,slurmd,slurmdbd,slurmrestd,example-configs} mpich mpich-devel mpich-doc @development gcc-gfortran hwloc pmix pam_script
 
 # install jupyterhub
-yum -y module install python39
-yum -y module enable python39-devel
-yum -y install python39-devel
+yum -y install python3.11 python3.11-pip python3.11-devel
 yum -y module install nodejs:18
 
 # install extra packages
--- common/jupyterhub_config.py
+++ common/jupyterhub_config.py
@@ -1152,7 +1152,7 @@ c.Spawner.default_url = '/lab'
 #  Note that this does *not* prevent users from accessing files outside of this
 #  path! They can do so with many other means.
 #  Default: ''
-# c.Spawner.notebook_dir = ''
+c.Spawner.notebook_dir = '~/tutorials'
 
 ## Allowed scopes for oauth tokens issued by this server's oauth client.
 #  
