b9f91a95417eca7a664bb51943911e94497b083b
Pun Wing Yin, Patrick <patrickwypun04@gmail.com>
2024-05-13 23:27:22 +0000

    openmpi submodule

--- common/openmpi-build.sh
+++ common/openmpi-build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
-cd /
-wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.6.tar.gz
-tar xvf openmpi-4.1.6.tar.gz
-cd openmpi-4.1.6
-./configure --prefix=/opt/openmpi --with-slurm --enable-shared --enable-mpi-fortran --enable-mpi-cxx --enable-mpi-cxx-seek --enable-orterun-prefix-by-default --with-hwloc --with-slurm --with-pmi --with-pmix
+# cd /
+# wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.6.tar.gz
+# tar xvf openmpi-4.1.6.tar.gz
+# cd openmpi-4.1.6
+cd ompi-src
+./autogen.pl
+./configure --prefix=/opt/openmpi --with-slurm --enable-shared --enable-mpi-fortran --enable-orterun-prefix-by-default --with-hwloc --with-pmix
 make -j 
 make install
