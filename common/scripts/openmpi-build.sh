f9621eb6b81003a5d94bef88e6221bee1a5842f6
Patrick Pun <patrickwypun04@gmail.com>
2026-02-18 00:00:46 +0000

    update el10 build still getting missing symbol error [skip ci]

--- common/scripts/openmpi-build.sh
+++ common/scripts/openmpi-build.sh
@@ -2,6 +2,6 @@
 
 cd ompi-src
 ./autogen.pl
-./configure --prefix=/opt/openmpi --with-slurm --enable-shared --enable-mpi-fortran --enable-orterun-prefix-by-default --with-hwloc --with-pmix --with-pmi2
+./configure --prefix=/opt/openmpi --with-slurm --enable-shared --enable-mpi-fortran --enable-orterun-prefix-by-default --with-hwloc --with-pmix=internal --with-pmi2
 make -j 
 make install
